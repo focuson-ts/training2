8112f8db527a9af494c53274d8a5d71205e6fd79
phil <phil.rice@validoc.org>
2022-04-05 17:50:28 +0200

    updated versions

--- src/focuson.generate.ts
+++ src/focuson.generate.ts
@@ -1,8 +1,7 @@
-import { AllGuardCreator, generate, makeButtons } from "@focuson/forms";
+import { AllGuardCreator, generate, makeButtons, params } from "@focuson/forms";
 import *  as fse from "fs-extra";
 import { devAppConfig, focusOnVersion, generatedPages, javaOutputRoot, tsRoot } from "./focuson.config";
 
-
-generate ( 'detailed', devAppConfig, javaOutputRoot, tsRoot, focusOnVersion, AllGuardCreator, makeButtons () ) ( generatedPages )
+generate ( 'detailed', devAppConfig, { ...params, focusOnVersion }, javaOutputRoot, tsRoot, AllGuardCreator, makeButtons () ) ( generatedPages )
 // fse.copySync ( 'src/components', tsRoot + "/src/components" )
 fse.copySync ( 'src/formComponents', tsRoot + "/src/formComponents" )
